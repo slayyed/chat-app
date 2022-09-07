03fde1e57bba2618f99624b82437418f6e8d919f
slayyed <xenxenoxbeats@gmail.com>
2022-09-07 22:24:32 +0400

    Classnames deleted from button component

--- components/Shared/Button/Button.tsx
+++ components/Shared/Button/Button.tsx
@@ -4,16 +4,14 @@ interface IButtonProps {
   children?: React.ReactNode;
   disabled?: boolean;
   type?: "button" | "submit";
-  className: string;
 }
 const Button: React.FC<IButtonProps> = ({
   children,
   type = "button",
-  className,
   ...props
 }) => {
   return (
-    <button {...props} className={styles.button + " " + className}>
+    <button {...props} className={styles.button}>
       {children}
     </button>
   );
