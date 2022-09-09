7ad6276a23dba8089a18f85bf767ac99f0272b39
slayyed <xenxenoxbeats@gmail.com>
2022-09-09 21:37:52 +0400

    Trasnfer app to cloud db

--- pre-build-scripts/getFileData.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var path = require("path");
-const file = path.join(__dirname, "../prisma/schema.prisma");
-fs.readFile(file, "utf8", function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
